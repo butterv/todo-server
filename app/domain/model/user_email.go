246deb742b4e288322f4a5e3d01ccf6483234460
istsh <github3104xniiapm@gmail.com>
2019-10-10 02:47:41 +0900

    Update user email model

--- app/domain/model/user_email.go
+++ app/domain/model/user_email.go
@@ -1,14 +1,25 @@
 package model
 
+// UserEmail is a structure with user email information.
 type UserEmail struct {
 	userID UserID
 	email  string
 }
 
+// NewUserEmail initializes UserEmail.
+func NewUserEmail(userID UserID, email string) *UserEmail {
+	return &UserEmail{
+		userID: userID,
+		email:  email,
+	}
+}
+
+// GetUserID gets a user id in UserEmail.
 func (u *UserEmail) GetUserID() UserID {
 	return u.userID
 }
 
+// GetEmail gets a email in UserEmail.
 func (u *UserEmail) GetEmail() string {
 	return u.email
 }
