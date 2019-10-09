ed7ef59a4448bc254b1b614ac3034fd70261c8e9
istsh <github3104xniiapm@gmail.com>
2019-10-10 02:49:01 +0900

    Define user usecase

---
+++ app/usecase/user_usecase.go
@@ -0,0 +1,12 @@
+package usecase
+
+import "github.com/istsh/todo-sample/app/domain/model"
+
+// UserUsecase is an user usecase.
+type UserUsecase interface {
+	RegisterUser(email, password string) (*model.User, error)
+	GetUser(userID model.UserID) (*model.User, error)
+	IsCorrectPassword(email, password string) (bool, error)
+	ChangeEmail(userID model.UserID, email string) error
+	ChangePassword(userID model.UserID, currentPassword, newPassword string) error
+}
