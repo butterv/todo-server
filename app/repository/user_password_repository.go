61c7cef241c1d4ed1df6b56cbbb4bc9813153121
istsh <github3104xniiapm@gmail.com>
2019-10-10 02:50:13 +0900

    Define user password repository

---
+++ app/repository/user_password_repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import "github.com/istsh/todo-sample/app/domain/model"
+
+// UserEmailRepositoryRefer is a readonly repository for user passwords.
+type UserPasswordRepositoryRefer interface {
+	FindByUserID(userID model.UserID) (*model.UserPassword, error)
+}
+
+// UserPasswordRepositoryModify is a read/write repository for user passwords.
+type UserPasswordRepositoryModify interface {
+	UserPasswordRepositoryRefer
+
+	Create(userID model.UserID, passwordHash string) (*model.UserPassword, error)
+	UpdatePasswordHashByUserID(userID model.UserID, passwordHash string) error
+	DeleteByUserID(userID model.UserID) error
+}
