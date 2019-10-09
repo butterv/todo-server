0913a3a728784efbbe80e63470f164994bacb64a
istsh <github3104xniiapm@gmail.com>
2019-10-10 02:47:28 +0900

    Update user model

--- app/domain/model/user.go
+++ app/domain/model/user.go
@@ -2,13 +2,23 @@ package model
 
 import "time"
 
+// UserID is type of id in User.
 type UserID string
 
+// User is a structure with user information.
 type User struct {
 	id        UserID
 	createdAt time.Time
 }
 
+// NewUser initializes User.
+func NewUser(id UserID) *User {
+	return &User{
+		id: id,
+	}
+}
+
+// GetID gets a id in User.
 func (u *User) GetID() UserID {
 	return u.id
 }
