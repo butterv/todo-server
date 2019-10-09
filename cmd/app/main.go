e24845db4160828df93f14112bc62282032511d5
istsh <github3104xniiapm@gmail.com>
2019-10-10 03:29:37 +0900

    WIP: implement main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
 
+	"google.golang.org/grpc"
+
+	todopb "github.com/istsh/todo-sample/app/proto/v1/todo"
+	userpb "github.com/istsh/todo-sample/app/proto/v1/user"
+	"github.com/istsh/todo-sample/app/repository"
+	"github.com/istsh/todo-sample/app/service/todoservice"
+	"github.com/istsh/todo-sample/app/service/userservice"
+)
+
+func main() {
+	// TODO: port should be configurable.
+	listenPort, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// TODO: implement repository
+	server := NewServer(nil)
+	server.Serve(listenPort)
+}
+
+func NewServer(r repository.Repository) *grpc.Server {
+	var ops []grpc.ServerOption
+	//ops = append(ops, grpc.UnaryInterceptor(auth.AuthenticationUnaryInterceptor()))
+	s := grpc.NewServer(ops...)
+
+	todoService := todoservice.New(r)
+	todopb.RegisterTodoServiceServer(s, todoService)
+
+	userService := userservice.New(r)
+	userpb.RegisterUserServiceServer(s, userService)
+
+	return s
 }
